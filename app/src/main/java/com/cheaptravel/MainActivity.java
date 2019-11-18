ee5d6d908785fc3c26b5f3bb59dd225858322b33
ChuHieu <chuhieu020297@gmail.com>
2019-11-18 15:21:29 +0700

    update 18-11

--- app/src/main/java/com/cheaptravel/MainActivity.java
+++ app/src/main/java/com/cheaptravel/MainActivity.java
@@ -1,14 +1,21 @@
 package com.cheaptravel;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Toast;
 
@@ -29,13 +36,70 @@ public class MainActivity extends AppCompatActivity implements FragmentUser.Frag
     private FragmentUser fragmentUser;
     private long backPresstime;
     private Toast backToast;
+    private static final int RC_STORAGE_PERMS1 = 101;
+    private static final int RC_STORAGE_PERMS2 = 102;
+    private int hasWriteExtStorePMS;
 
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case RC_STORAGE_PERMS1:
+            case RC_STORAGE_PERMS2:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (requestCode == RC_STORAGE_PERMS1) {
+                    } else {
+//                        startActivity(new Intent(this, DownloadActivity.class));
+                    }
+                    return;
+                }
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setMessage("Bạn cần cấp quyền để sử dụng ứng dụng");
+                alert.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        intent.setData(Uri.parse("package:" + getPackageName()));
+                        startActivityForResult(intent, requestCode);
+                    }
+                });
+                alert.setCancelable(false);
+                alert.show();
+                break;
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RC_STORAGE_PERMS1:
+            case RC_STORAGE_PERMS2:
+                hasWriteExtStorePMS = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (hasWriteExtStorePMS == PackageManager.PERMISSION_GRANTED) {
+                    if (requestCode == RC_STORAGE_PERMS1) {
+                    } else {
+//                        startActivity(new Intent(this, DownloadActivity.class));
+                    }
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, RC_STORAGE_PERMS2);
+                }
+                break;
+        }
+    }
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hasWriteExtStorePMS = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (hasWriteExtStorePMS == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, RC_STORAGE_PERMS1);
+        }
+
 
         this.fragmentHome = new FragmentHome();
         this.fragmentPlace = new FragmentPlace();
--- app/src/main/java/com/cheaptravel/fragment/FragmentUser.java
+++ app/src/main/java/com/cheaptravel/fragment/FragmentUser.java
@@ -1,17 +1,21 @@
 package com.cheaptravel.fragment;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
@@ -33,6 +37,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
@@ -82,17 +88,16 @@ public class FragmentUser extends Fragment implements View.OnClickListener {
 
 
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
         mStorage = FirebaseStorage.getInstance().getReference();
-
         map(view);
         new getUser().execute();
         return view;
     }
-
 
     private String getKeyUser() {
         String key = "";
